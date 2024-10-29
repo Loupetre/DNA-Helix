c1fa4e17dbad4376ec50f9edfc3636e18a9a7191
Loupetre <pierre.liegeois@wanadoo.fr>
2024-10-29 11:59:47 +0100

    commit du midi

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #define OUT2 9                      // pin led strip 2
 Adafruit_NeoPixel helice1 = Adafruit_NeoPixel(11,OUT1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel helice2 = Adafruit_NeoPixel(11,OUT2, NEO_GRB + NEO_KHZ800);
+int delai = 0;                      // delai entre chaque mise à jour allumage des bandes led
 
 #define SD_CS 10                    // pin CS pour le lecteur SD
 Sd2Card CarteSD;
@@ -17,6 +18,7 @@ SdFile root;
 uint32_t volumesize;
 #define nomDuFichier "genome.txt" 
 File genome_file;
+bool fromSD;                        // pas de SD on va afficher des bases au pif
 
 int irInput = 7;                    // pin pour le récepteur IR
 int modePin = 5;                    // pour le bouton poussoir de sélection du mode
@@ -37,6 +39,11 @@ int counter = 0;
 boolean buttonReady = true;
 int position = 0;
 
+bool get_carte_SD();
+void fill_adn_tab(char);
+void lights_up(int);
+void decalage();
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Let's begin !!");
@@ -48,41 +55,31 @@ void setup() {
   irrecv.enableIRIn();
   randomSeed(analogRead(A0));
 
-  // init du tableau des couleurs des hélices
-  for(int k=0;k<2;k++)
-      for(int i=0;i<12;i++)
-        for(int j=0;j<3;j++)
-        adn[k][i][j] = 0;
-    
-  
   // carte SD
   pinMode(10, OUTPUT);
   SD.begin(SD_CS);
-  if (!CarteSD.init(SPI_HALF_SPEED, SD_CS)) {
-    Serial.println(F("Échec lors de l'initialisation du lecteur SD "));
-    Serial.println();
-  } else {
-    Serial.println(F("Carte SD trouvée."));
-    Serial.println();
-    if (!VolumeCarteSD.init(CarteSD)) {
-      Serial.println(F("Aucune partition FAT16/FAT32 trouvée."));
-      Serial.println(F("Vérifiez si votre carte SD est bien formatée !"));
-      } else {
-        Serial.print(F("Format du volume : FAT"));
-        Serial.println(VolumeCarteSD.fatType(), DEC);
-        root.openRoot(VolumeCarteSD);
-        root.ls(LS_R | LS_DATE | LS_SIZE);
-        }
-  }
 }
+
+// ********************************************* LOOP ******************************************
+// *********************************************************************************************
 
 void loop() {
   // quelques initialisations de variables
-  position_helice = 0;    // premier étage
-  // lecture du fichier genome.txt
-  Serial.print("Lecture du fichier génome : ");Serial.println(nomDuFichier);
-  genome_file = SD.open(nomDuFichier, FILE_READ);
-  if (genome_file) {
+  position_helice = 0;            // premier étage
+  for(int k=0;k<2;k++)            // init du tableau des couleurs des hélices
+      for(int i=0;i<11;i++)
+        for(int j=0;j<3;j++)
+        adn[k][i][j] = 0;
+  fromSD = get_carte_SD();          // init SD. Indique si on a réussi l'init
+
+  if (fromSD) {
+    // lecture du fichier genome.txt
+    Serial.print("Lecture du fichier génome : ");
+    Serial.println(nomDuFichier);
+    genome_file = SD.open(nomDuFichier, FILE_READ);
+    fromSD = genome_file;          // indique si on a réussi à lire le fichier
+  }
+  if (fromSD) {
     // lecture de la première ligne qui indique de quel génome il s'agit
     Serial.println(genome_file.readStringUntil('\n'));  // lecture de la première ligne - le titre -
     while (genome_file.available()) {             // Lecture du reste : il faut déterminer le complément
@@ -90,72 +87,28 @@ void loop() {
       counter++;
       if (base != '\n' && base != '\r')
       {
-        switch (base)               // calcul du complément aux bases ATCG
-        {
-        case 'a':
-          base = 'A';
-        case 'A':
-          base2 = 'T';
-          // on met la couleur correspondante dans le tableau ADN en position courante
-          adn[0][0][0] = adenine_color[0];
-          adn[0][0][1] = adenine_color[1];
-          adn[0][0][2] = adenine_color[2];
-          adn[1][0][0] = thymine_color[0];
-          adn[1][0][1] = thymine_color[1];
-          adn[1][0][2] = thymine_color[2];
-          break;
-        case 't':
-          base = 'T';
-        case 'T':
-          base2 = 'A';
-          // on met la couleur correspondante dans le tableau ADN en position courante
-          adn[0][0][0] = thymine_color[0];
-          adn[0][0][1] = thymine_color[1];
-          adn[0][0][2] = thymine_color[2];
-          adn[1][0][0] = adenine_color[0];
-          adn[1][0][1] = adenine_color[1];
-          adn[1][0][2] = adenine_color[2];
-          break;
-        case 'c':
-          base = 'C';
-        case 'C':
-          base2 = 'G';
-          // on met la couleur correspondante dans le tableau ADN en position courante
-          adn[0][0][0] = cytosine_color[0];
-          adn[0][0][1] = cytosine_color[1];
-          adn[0][0][2] = cytosine_color[2];
-          adn[1][0][0] = guanine_color[0];
-          adn[1][0][1] = guanine_color[1];
-          adn[1][0][2] = guanine_color[2];
-          break;
-        case 'g':
-          base = 'G';
-        case 'G':
-          base2 = 'C';
-          // on met la couleur correspondante dans le tableau ADN en position courante
-          adn[0][0][0] = guanine_color[0];
-          adn[0][0][1] = guanine_color[1];
-          adn[0][0][2] = guanine_color[2];
-          adn[1][0][0] = cytosine_color[0];
-          adn[1][0][1] = cytosine_color[1];
-          adn[1][0][2] = cytosine_color[2];
-          break;
-        default:
-          break;
-        }
+        fill_adn_tab(base);
         //Serial.print(base); Serial.println(base2);delay(250);
-        // on allume les leds !!
-        // décalage des leds pour la prochaine lecture
+        lights_up(delai);                // on allume les leds !! Le délai est réglable avec le potar
+        decalage();                    // décalage des leds pour la prochaine lecture
       }
     }
     genome_file.close();  
     Serial.print("Nombre de bases lues : ");
     Serial.println(counter);        
     counter = 0;
-  } else
-  {
-    Serial.println("Fichier génome non trouvé");
-    while(1);
+  } else {
+    Serial.println("Fichier génome non trouvé");  // on fait clignoter les hélices en rouge du coup 
+    for (int i=0; i<100; i++)
+    {
+      if (i % 2 == 0) {
+        fill_adn_tab('X');              // code X spécial (rouge voir fonction fill_adn_tab)
+      } else {
+        fill_adn_tab('Z');              // code spécial (pas de couleur)
+      }
+      lights_up(delai);                  // on allume les leds !! Le délai est réglable avec le potar
+      decalage();                      // décalage des leds pour la prochaine lecture
+    }
   }
 
   if(irrecv.decode(&results)){
@@ -169,13 +122,127 @@ void loop() {
     }
     irrecv.resume();
   }
-  for(int i=0;i<12;i++){
-    //helice1.setPixelColor(i,helice1.Color(r,g,b)); 
-  } 
-  //helice1.show();
-  //helice2.show();
 
   position+= (analogRead(A0)/128.0);    
   
   delay(20);
+}
+
+// ********************************************* FUNCTIONS *************************************
+// *********************************************************************************************
+
+bool get_carte_SD() {
+  fromSD = true;
+  if (!CarteSD.init(SPI_HALF_SPEED, SD_CS)) {                     // init du lecteur de carte SD
+    Serial.println(F("Échec lors de l'initialisation du lecteur SD "));
+    return false;
+    } else {
+      Serial.println(F("Carte SD trouvée."));
+      if (!VolumeCarteSD.init(CarteSD)) {
+        Serial.println(F("Aucune partition FAT16/FAT32 trouvée."));
+        Serial.println(F("Vérifiez si votre carte SD est bien formatée !"));
+        return false;
+        } else {
+          Serial.print(F("Format du volume : FAT"));
+          Serial.println(VolumeCarteSD.fatType(), DEC);
+          root.openRoot(VolumeCarteSD);
+          root.ls(LS_R | LS_DATE | LS_SIZE);
+          }
+    }
+    return true;
+}
+
+void fill_adn_tab(char base) {
+  switch (base)               // calcul du complément aux bases ATCG
+  {
+    case 'a':
+      base = 'A';
+    case 'A':
+      base2 = 'T';
+      // on met la couleur correspondante dans le tableau ADN en position courante
+      adn[0][0][0] = adenine_color[0];
+      adn[0][0][1] = adenine_color[1];
+      adn[0][0][2] = adenine_color[2];
+      adn[1][0][0] = thymine_color[0];
+      adn[1][0][1] = thymine_color[1];
+      adn[1][0][2] = thymine_color[2];
+      break;
+    case 't':
+      base = 'T';
+    case 'T':
+      base2 = 'A';
+      // on met la couleur correspondante dans le tableau ADN en position courante
+      adn[0][0][0] = thymine_color[0];
+      adn[0][0][1] = thymine_color[1];
+      adn[0][0][2] = thymine_color[2];
+      adn[1][0][0] = adenine_color[0];
+      adn[1][0][1] = adenine_color[1];
+      adn[1][0][2] = adenine_color[2];
+      break;
+    case 'c':
+      base = 'C';
+    case 'C':
+      base2 = 'G';
+      // on met la couleur correspondante dans le tableau ADN en position courante
+      adn[0][0][0] = cytosine_color[0];
+      adn[0][0][1] = cytosine_color[1];
+      adn[0][0][2] = cytosine_color[2];
+      adn[1][0][0] = guanine_color[0];
+      adn[1][0][1] = guanine_color[1];
+      adn[1][0][2] = guanine_color[2];
+      break;
+    case 'g':
+      base = 'G';
+    case 'G':
+      base2 = 'C';
+      // on met la couleur correspondante dans le tableau ADN en position courante
+      adn[0][0][0] = guanine_color[0];
+      adn[0][0][1] = guanine_color[1];
+      adn[0][0][2] = guanine_color[2];
+      adn[1][0][0] = cytosine_color[0];
+      adn[1][0][1] = cytosine_color[1];
+      adn[1][0][2] = cytosine_color[2];
+      break;
+    case 'X':
+      // on met en rouge les deux bases
+      adn[0][0][0] = 255;
+      adn[0][0][1] = 0;
+      adn[0][0][2] = 0;
+      adn[1][0][0] = 255;
+      adn[1][0][1] = 0;
+      adn[1][0][2] = 0;
+      break;
+    case 'Z':
+      // on met en "noir" les deux bases
+      adn[0][0][0] = 0;
+      adn[0][0][1] = 0;
+      adn[0][0][2] = 0;
+      adn[1][0][0] = 0;
+      adn[1][0][1] = 0;
+      adn[1][0][2] = 0;
+      break;
+    default:
+      break;
+    }
+}
+
+void lights_up(int valeur_delai) {
+  for(int i=0;i<11;i++){
+    helice1.setPixelColor(i,helice1.Color(adn[0][i][0],adn[0][i][1],adn[0][i][2]));
+    helice1.setPixelColor(i,helice1.Color(adn[1][i][0],adn[1][i][1],adn[1][i][2]));
+  } 
+  helice1.show();
+  helice2.show();
+  delay(valeur_delai);
+}
+
+void decalage() {
+  for (int i=10;i<1;i--) {
+    adn[0][i][0] = adn[0][i-1][0];
+    adn[0][i][1] = adn[0][i-1][1];
+    adn[0][i][2] = adn[0][i-1][2];
+    adn[1][i][0] = adn[1][i-1][0];
+    adn[1][i][1] = adn[1][i-1][1];
+    adn[1][i][2] = adn[1][i-1][2];
+  }
 }
